d91fb487031499038057a9d369a92b4ff319018b
Satvik Tiwari <74293601+satviktiwari@users.noreply.github.com>
2021-06-20 17:21:23 +0530

    File Updated

--- emai.R
+++ emai.R
@@ -1,14 +1,11 @@
 emailing <- function(to, from, subject, text, password){
-  
   library(emayili)
-  
   email <- envelope(
     to = to,
     from = from,
     subject = subject,
     text = text
   )
-  
   smtp <- server(host = "smtp.gmail.com",
                  port = 465,
                  username = from,
